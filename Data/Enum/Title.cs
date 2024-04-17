d25a10f18fc69e61bccf4b270445071f4609aa8e
aatlic1 <aatlic1@etf.unsa.ba>
2024-04-17 12:19:46 +0200

    creating models

---
+++ Data/Enum/Title.cs
@@ -0,0 +1,9 @@
+﻿namespace Clinic.Data.Enum
+{
+    public enum Title
+    {
+        Specialist,
+        Resident,
+        Nurse
+    }
+}
---
+++ Models/Patient.cs
@@ -0,0 +1,20 @@
+﻿using Clinic.Data.Enum;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Clinic.Models
+{
+    public class Patient
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateOnly BirthDate  { get; set; }
+        public Gender Gender { get; set; }
+        [ForeignKey("Address")]
+        public int? AddressId { get; set; }
+        public Address? Address { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+}
