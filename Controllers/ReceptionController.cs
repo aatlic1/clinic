4558fc240a30a1005701407e52da2016bd58f09f
aatlic1 <aatlic1@etf.unsa.ba>
2024-04-17 17:54:41 +0200

    reception view

--- Controllers/ReceptionController.cs
+++ Controllers/ReceptionController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Clinic.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Clinic.Controllers
 {
     public class ReceptionController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public ReceptionController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var receptions = _context.Receptions.Include(p => p.Patient).Include(d => d.Doctor).ToList();
+            return View(receptions);
         }
     }
 }
