18f69eb329ae33da44f5d56a7c68df3a833d93de
aatlic1 <aatlic1@etf.unsa.ba>
2024-04-17 18:53:11 +0200

    creating detail page for patient

--- Controllers/PatientController.cs
+++ Controllers/PatientController.cs
@@ -1,5 +1,7 @@
 ﻿using Clinic.Data;
+using Clinic.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Clinic.Controllers
 {
@@ -15,6 +17,11 @@ namespace Clinic.Controllers
         {
             var patients = _context.Patients.ToList();
             return View(patients);
+        }
+        public IActionResult Detail(int id)
+        {
+            Patient patient = _context.Patients.Include(a => a.Address).FirstOrDefault(p => p.Id == id); 
+            return View(patient);
         }
     }
 }
