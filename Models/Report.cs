6545d0315238c0ac517386b969585ace644d484d
aatlic1 <aatlic1@etf.unsa.ba>
2024-04-17 12:19:46 +0200

    creating models

---
+++ Models/Report.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Clinic.Models
+{
+    public class Report
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("Patient")]
+        public int PatientId { get; set; }
+        public Patient Patient { get; set; }
+    }
+}
