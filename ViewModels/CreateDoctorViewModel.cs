fabe61e603d6ba800153b7fd9e6a624506b21518
aatlic1 <aatlic1@etf.unsa.ba>
2024-04-19 12:27:02 +0200

    creating viewmodels and fixing code in controllers

---
+++ ViewModels/CreateDoctorViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Clinic.Data.Enum;
+
+namespace Clinic.ViewModels
+{
+    public class CreateDoctorViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public Title Title { get; set; }
+        public string Code { get; set; }
+    }
+}
