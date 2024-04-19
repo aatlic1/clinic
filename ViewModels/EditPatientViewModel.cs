09a63f54d23c9f484cf11939d67cc63890e8f529
aatlic1 <aatlic1@etf.unsa.ba>
2024-04-19 12:49:29 +0200

    edit page for patient

---
+++ ViewModels/EditPatientViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Clinic.Data.Enum;
+using Clinic.Models;
+
+namespace Clinic.ViewModels
+{
+    public class EditPatientViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public Gender Gender { get; set; }
+        public int? AddressId { get; set; }
+        public Address? Address { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+}
