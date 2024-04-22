ebf777671a62aca79145d49f9f5df233f5f3ca1d
aatlic1 <aatlic1@etf.unsa.ba>
2024-04-22 14:40:20 +0200

    showing all reports for pacient

--- Repository/ReportRepository.cs
+++ Repository/ReportRepository.cs
@@ -1,6 +1,8 @@
 ﻿using Clinic.Data;
 using Clinic.Interfaces;
 using Clinic.Models;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
 
 namespace Clinic.Repository
 {
@@ -23,6 +25,10 @@ namespace Clinic.Repository
         {
             var saved = _context.SaveChanges();
             return saved > 0 ? true : false;
+        }
+        public async Task<IEnumerable<Report>> GetReportsByPatientId(int id)
+        {
+            return await _context.Reports.Where(r => r.PatientId == id).ToListAsync();
         }
     }
 }
