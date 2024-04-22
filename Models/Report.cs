b12c672ac055e4b6e2b2eba18a66243a157e57bc
aatlic1 <aatlic1@etf.unsa.ba>
2024-04-22 10:38:02 +0200

    editing entity reports, adding more atributes

--- Models/Report.cs
+++ Models/Report.cs
@@ -10,5 +10,11 @@ namespace Clinic.Models
         [ForeignKey("Patient")]
         public int PatientId { get; set; }
         public Patient Patient { get; set; }
+        [ForeignKey("Doctor")]
+        public string DoctorId { get; set; }
+        public Doctor Doctor { get; set; }
+        public string Description { get; set; }
+        public string Caption { get; set; }
+        public DateTime DateTime { get; set; }
     }
 }
