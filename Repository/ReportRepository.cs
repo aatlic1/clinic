342111bc4fbe5b3259cfeb6e8d87a5d5496f6384
aatlic1 <aatlic1@etf.unsa.ba>
2024-04-22 11:02:17 +0200

    repository for medical report

---
+++ Repository/ReportRepository.cs
@@ -0,0 +1,28 @@
+﻿using Clinic.Data;
+using Clinic.Interfaces;
+using Clinic.Models;
+
+namespace Clinic.Repository
+{
+    public class ReportRepository: IReportRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReportRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool Add(Report report)
+        {
+            _context.Add(report);
+            return Save();
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+    }
+}
